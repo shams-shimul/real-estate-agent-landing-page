55ced0fabfa1eae8d09bb3bf5d375f965ede9671
Shams Shimul <65963238+shams-shimul@users.noreply.github.com>
2020-12-11 19:07:11 +0600

    added scrolling animations and modal image viewer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,49 @@
+$(window).scroll(function () {
+  $(".slideanim1").each(function () {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+  $(".slideanim2").each(function () {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      setTimeout(() => {
+        $(this).addClass("slide");
+      }, 150);
+    }
+  });
+  $(".slideanim3").each(function () {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      setTimeout(() => {
+        $(this).addClass("slide");
+      }, 300);
+    }
+  });
+  $(".slideanim4").each(function () {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      setTimeout(() => {
+        $(this).addClass("slide");
+      }, 450);
+    }
+  });
+  $(".slideanim5").each(function () {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      setTimeout(() => {
+        $(this).addClass("slide");
+      }, 600);
+    }
+  });
+});
+
 $(document).ready(function () {
   $('.testimonial-slider').slick({
     infinite: true,
@@ -14,4 +60,23 @@ $(document).ready(function () {
       }
     ]
   });
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal
+  var modalImg = $("#modal-img");
+  $(".srcImg").on("click", function() {
+    url = $(this).attr("src");
+    modal.style.display = "block";
+    $(modalImg).attr("src", url);
+  })
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
 });
